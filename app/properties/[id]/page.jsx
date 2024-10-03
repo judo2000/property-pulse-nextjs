5fd95d15c4420bb16830ab2552a7d288aca1c8cd
Stephen Moore <judo2000@comcast.net>
2024-10-03 17:54:34 -0600

    Build out main section of PropertyDetails page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,3 +1,4 @@
+import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -21,8 +22,11 @@ const AddPropertyPage = async ({ params }) => {
         </div>
       </section>
       <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6"></div>
-        <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+        <div class="container m-auto py-10 px-6">
+          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
+        </div>
       </section>
     </>
   );
