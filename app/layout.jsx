b3d817435560eba21f76f92269247dc12aa288a3
Stephen Moore <judo2000@comcast.net>
2024-10-03 10:26:55 -0600

    Add Theme files and and move images to assets/images and public folders

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
 const MainLayout = ({ children }) => {
   return (
     <html>
