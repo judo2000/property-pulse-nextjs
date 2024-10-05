d13c280d1cc010e5af1fb36f3939300e935d94eb
Stephen Moore <judo2000@comcast.net>
2024-10-04 19:13:32 -0600

    Display google profile picture in the navbar when logged in

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,8 +11,8 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const profileImage = session?.user?.image;
 
-  console.log(session);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [providers, setProviders] = useState(null);
@@ -165,7 +165,9 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
+                      width={40}
+                      height={40}
                       alt=""
                     />
                   </button>
